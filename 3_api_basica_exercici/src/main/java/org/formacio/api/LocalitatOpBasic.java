73cb4ef7a83088b6dc90b2352d65ee8951d91f35
marc monjon <sysmarc13@hotmail.com>
2019-05-13 21:03:44 +0200

    Peninultimo caso test pasado

--- 3_api_basica_exercici/src/main/java/org/formacio/api/LocalitatOpBasic.java
+++ 3_api_basica_exercici/src/main/java/org/formacio/api/LocalitatOpBasic.java
@@ -7,35 +7,37 @@ import javax.transaction.Transactional;
 import org.springframework.stereotype.Component;
 
 /**
- * Modifica aquesta classe per tal que sigui un component Spring que realitza les 
- * operacions de persistencia tal com indiquen les firmes dels metodes
+ * Modifica aquesta classe per tal que sigui un component Spring que realitza
+ * les operacions de persistencia tal com indiquen les firmes dels metodes
  */
 @Component
 public class LocalitatOpBasic {
-	
+
 	@PersistenceContext
-    private EntityManager entityManager = null;
+	private EntityManager entityManager = null;
 	Localitat localitat = new Localitat();
-	
-	public Localitat carrega (long id) {
+
+	public Localitat carrega(long id) {
 		Localitat localitat = entityManager.find(Localitat.class, id);
-        return localitat;
+		return localitat;
 	}
-	
+
 	@Transactional
-	public void alta (String nom, Integer habitants) {
+	public void alta(String nom, Integer habitants) {
 		localitat.setNom(nom);
 		localitat.setHabitants(habitants);
 		entityManager.persist(localitat);
 	}
-	
+
 	@Transactional
-	public void elimina (long id) {
-		Localitat eliminarLocalitat = this.carrega(id);
-		entityManager.remove(eliminarLocalitat);
+	public void elimina(long id) {
+		Localitat localitat = this.carrega(id);
+		if (localitat != null) {
+			entityManager.remove(localitat);
+		}
 	}
-	
-	public void modifica (Localitat localitat) {
+
+	public void modifica(Localitat localitat) {
 	}
 
 }
