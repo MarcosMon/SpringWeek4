d8d8931d6d2b9285fe026d604931a41a84daf81a
marc monjon <sysmarc13@hotmail.com>
2019-05-18 17:29:53 +0200

    Completo

--- 5_setmana_1_exercici/src/main/java/org/formacio/setmana1/data/LlibreOpsBasic.java
+++ 5_setmana_1_exercici/src/main/java/org/formacio/setmana1/data/LlibreOpsBasic.java
@@ -101,8 +101,19 @@ public class LlibreOpsBasic {
 	 * Retorna quina es la recomanacio per el llibre indicat Si el llibre indicat no
 	 * existeix, retorna null
 	 */
-	public Recomanacio recomenacioPer(String isbn) {
-		return null;
+	
+	public Recomanacio recomenacioPer (String isbn) {
+		if(this.existeix(isbn)){
+			 try {
+				llibre = this.carrega(isbn);
+			} catch (LlibreNoExisteixException e) {
+				e.printStackTrace();
+			}
+			return llibre.getRecomanacio();
+		}
+		else {
+			return null;
+		}		
 	}
 
 }
